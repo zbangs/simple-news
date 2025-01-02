7162dfdc20837e3875bb1d4e49798612d409e25b
Zackery Bangs <zackerybangs@gmail.com>
2025-01-02 00:37:37 +0000

    Add project files

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "dotenv/load"
+require "http"
+require "json"
+
+news_api_key = ENV.fetch("NEWS_API_KEY")
+news_api_url = "https://newsapi.org/v2"
+
+set :public_folder, __dir__ + '/'
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  redirect("/top-headlines")
+end
+
+get("/top-headlines") do
+  news_api_url += "/top-headlines?"
+
+  erb(:top_headlines)
+end
+
+get("/random") do
+  erb(:random_result)
+end
+
+get("/search-result") do
+  news_api_url += "/everything?"
+
+  erb(:search_result)
 end
